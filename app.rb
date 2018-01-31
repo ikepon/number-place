bfd7e1dbd719ea532a7fb2a8362981a5247f00fa
ikepon <tatsuyanoheya@gmail.com>
2018-01-31 18:45:27 +0900

    feat: 行、列、3x3正方形を取得

--- app.rb
+++ app.rb
@@ -2,6 +2,33 @@ class Table
   def initialize(number_table)
     @table = number_table
   end
+
+  def table
+    @table
+  end
+
+  def row(n)
+    @table[n - 1]
+  end
+
+  def column(n)
+    @table.transpose[n - 1]
+  end
+
+  # 3x3の正方形は左上から右に
+  #   1,2,3
+  #   4,5,6
+  #   7,8,9
+  # とする
+  def square(n)
+    div, mod = (n - 1).divmod(3)
+
+    [
+      @table[div * 3][(mod * 3)..(mod * 3 + 2)],
+      @table[div * 3 + 1][(mod * 3)..(mod * 3 + 2)],
+      @table[div * 3 + 2][(mod * 3)..(mod * 3 + 2)]
+    ]
+  end
 end
 
 table = [
@@ -17,3 +44,15 @@ table = [
 ]
 
 np = Table.new(table)
+
+print np.row(1)
+print np.row(4)
+print np.row(8)
+
+print np.column(1)
+print np.column(4)
+print np.column(8)
+
+print np.square(5)
+print np.square(7)
+print np.square(9)
